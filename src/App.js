ed85b0f45694cb68a10a383a3d10fa5951b899a9
Oliver Gomes <ogomesphoto@gmail.com>
2019-03-27 00:46:01 -0400

    Rental comp add

--- src/App.js
+++ src/App.js
@@ -1,12 +1,24 @@
 import React, { Component } from "react";
 import Header from "./shared/Header";
 import "./App.css";
+import RentalCard from "./components/RentalCard";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <Header />
+
+        <div className="container">
+          <section id="rentalListing">
+            <h1 className="page-title">Your Home All Around the World</h1>
+            <div className="row">
+              <RentalCard />
+              <RentalCard />
+              <RentalCard />
+            </div>
+          </section>
+        </div>
       </div>
     );
   }
