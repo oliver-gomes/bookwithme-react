255bad33a41015593fad76df363de11c38b52006
Oliver Gomes <ogomesphoto@gmail.com>
2019-03-27 13:47:41 -0400

    rental detail add

---
+++ src/components/rental/RentalDetail.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+export class RentalDetail extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>I am Rental Detail</h1>
+        <h1>I am Rental Detail</h1>
+        <h1>I am Rental Detail</h1>
+        <h1>I am Rental Detail</h1>
+      </div>
+    );
+  }
+}
