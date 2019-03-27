a6f5cc5d972adcfdfb9a7b1a144901bb7991cd5d
Oliver Gomes <ogomesphoto@gmail.com>
2019-03-27 13:47:07 -0400

    React router update

--- src/App.js
+++ src/App.js
@@ -1,19 +1,24 @@
 import React, { Component } from "react";
-import Header from "./shared/Header";
-import "./App.css";
+import { BrowserRouter, Route } from "react-router-dom";
 
+import Header from "./shared/Header";
 import RentalList from "./components/rental/RentalList";
+import { RentalDetail } from "./components/rental/RentalDetail";
+
+import "./App.css";
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <Header />
-
-        <div className="container">
-          <RentalList />
+      <BrowserRouter>
+        <div className="App">
+          <Header />
+          <div className="container">
+            <Route exact path="/" component={RentalList} />
+            <Route exact path="/test" component={RentalDetail} />
+          </div>
         </div>
-      </div>
+      </BrowserRouter>
     );
   }
 }
